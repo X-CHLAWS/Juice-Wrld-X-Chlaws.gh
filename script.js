0d8d2e3b51c49f974ed14e4ba81015a8b2bb06f8
X-CHLAWS <acheampongnhyira072@gmail.com>
2024-12-05 13:41:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToMusic() {
+    document.getElementById("music").scrollIntoView({ behavior: 'smooth' });
+}
+// Add event listener to navigation links
+document.querySelectorAll('nav a').forEach(link => {
+link.addEventListener('click', event => {
+event.preventDefault();
+const target = document.querySelector(link.getAttribute('href'));
+target.scrollIntoView({ behavior: 'smooth' });
+});
+});
